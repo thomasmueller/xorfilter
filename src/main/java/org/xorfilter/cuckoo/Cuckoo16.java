b7775a022753b344c16b246205a4facb51d9d83a
Thomas Mueller <mueller@adobe.com>
2018-11-06 08:29:36 +0100

    Improved cuckoo filters

--- src/main/java/org/xorfilter/cuckoo/Cuckoo16.java
+++ src/main/java/org/xorfilter/cuckoo/Cuckoo16.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017 Randall Jones
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.xorfilter.cuckoo;
 
 import java.util.Random;
@@ -39,11 +24,17 @@ public class Cuckoo16 implements Filter {
 
     public static Cuckoo16 construct(long[] keys) {
         int len = keys.length;
-        Cuckoo16 f = new Cuckoo16((int) (len / 0.95));
-        for (long k : keys) {
-            f.insert(k);
+        while (true) {
+            try {
+                Cuckoo16 f = new Cuckoo16((int) (len / 0.95));
+                for (long k : keys) {
+                    f.insert(k);
+                }
+                return f;
+            } catch (IllegalStateException e) {
+                // table full: try again
+            }
         }
-        return f;
     }
 
     public Cuckoo16(int capacity) {
